ae27be54fbcfb8fdb00209f84bf95458cd57f2e8
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-02-05 03:32:12 +0100

    create operate function with switch case

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 //
 //create functions for handling basic math
 // (add,subtract,multiply,divide)
+//create variables for storing operators, number x and y
+//create a function operate that takes an operator and operands then calls one of the arithmetic functions
 //
+let operator = "";
+let numX = 0;
+let numY = 0;
+
+
+//====basic arithmetic functions====
 function add(a,b){
     return a + b
 }
@@ -17,4 +25,25 @@ function multiply(a,b){
 
 function divide(a,b){
     return a / b
+}
+
+//====end basic arithmetic functions====
+
+//====operate function that calls arithmetic function====
+//
+function operate(operandX, operandY){
+    switch(operator){
+        case  "multiply":
+              return multiply(operandX, operandY)
+        case  "divide":
+              return divide(operandX, operandY)
+        case  "subtract":
+              return subtract(operandX, operandY)
+        case  "add":
+              return add(operandX, operandY)
+        default:
+            console.log("invalid operator/no operator have been specified")
+}
+
+
 }
